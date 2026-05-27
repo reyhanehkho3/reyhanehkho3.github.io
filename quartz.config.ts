55fea0f2c2794b9c05fe7f92bba0ffdae2242ea5
Reyhaneh <rkhosravi8506@gmail.com>
2026-05-27 16:51:59 +0330

    added icon and changed base name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Knowledge Base",
+    pageTitle: "reyhan base",
+    favicon: "static/icon.png",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
